5d91db61418ab08b05752ad98dbac31d6c508c8b
Jeff L <jeff@langrsoft.com>
2021-04-18 17:33:03 -0400

    solution

--- src/misc/name-normalizer.js
+++ src/misc/name-normalizer.js
@@ -1,17 +1,47 @@
-// this will likeley prove useful and you won't
-// have to find it on StackOverflow:
-//const numberOfCharactersInString = (s, char) =>
-//  (s.match(new RegExp(char, 'g'))||[]).length;
+const numberOfCharactersInString = (s, char) =>
+  (s.match(new RegExp(char, 'g'))||[]).length;
 
 const parts = name => name.split(' ')
 
-const last = name => parts(name)[1]
+const last = name => name.parts[name.parts.length - 1]
 
-const first = name => parts(name)[0]
+const first = name => name.parts[0]
 
-const isMononym = name => parts(name).length === 1
+const isMononym = name => name.parts.length === 1
+
+const isDuonym = name => name.parts.length === 2
+
+const middleInitials = name =>
+  name.parts
+    .slice(1, -1)
+    .map(part => initial(part))
+    .join(' ')
+
+const initial = namePart =>
+  namePart.length === 1 ? namePart : `${namePart[0]}.`
+
+const suffix = nameParse =>
+  nameParse.suffix ? `,${nameParse.suffix}` : ''
+
+const throwOnExcessCommas = name => {
+  if (numberOfCharactersInString(name, ',') > 1)
+    throw Error()
+}
+
+const nameParse = name => {
+  const [baseName, suffixPart] = name.trim().split(',')
+  return {
+    name: name,
+    parts: parts(baseName),
+    baseName: baseName,
+    suffix: suffixPart
+  }
+}
 
 export const normalize = name => {
-  if (isMononym(name)) return name
-  return `${last(name)}, ${first(name)}`
+  throwOnExcessCommas(name)
+  const parsed = nameParse(name)
+  if (isMononym(parsed)) return parsed.baseName
+  if (isDuonym(parsed)) return `${last(parsed)}, ${first(parsed)}`
+  return `${last(parsed)}, ${first(parsed)} ${middleInitials(parsed)}${suffix(parsed)}`
 }
--- src/misc/name-normalizer.test.js
+++ src/misc/name-normalizer.test.js
@@ -13,7 +13,27 @@ describe('a name normalizer', () => {
     expect(normalize('Haruki Murakami')).toEqual('Murakami, Haruki')
   })
 
-  xit('trims leading and trailing whitespace', () => {
+  it('trims leading and trailing whitespace', () => {
     expect(normalize('  Big Boi   ')).toEqual('Boi, Big')
+  })
+
+  it('initializes middle names', () => {
+    expect(normalize('Phineas Taylor Barnum')).toEqual('Barnum, Phineas T.')
+  })
+
+  it('does not initialize single-letter middle names', () => {
+    expect(normalize('Harry S Truman')).toEqual('Truman, Harry S')
+  })
+
+  it('initializes multiple middle names', () => {
+    expect(normalize('Julia Scarlett Elizabeth Louis-Dreyfus')).toEqual('Louis-Dreyfus, Julia S. E.')
+  })
+
+  it('appends suffixes', () => {
+    expect(normalize('Martin Luther King, Jr.')).toEqual('King, Martin L., Jr.')
+  })
+
+  it('throws on too many commas', () => {
+    expect(() => { normalize('Martin, Luther King, Jr.') }).toThrow()
   })
 })
