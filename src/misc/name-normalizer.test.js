e7cb3b9c0d647fc0926603587cf8ded9e750687a
Michael Antry <michael.antry.ext@bayer.com>
2021-04-19 10:39:31 -0500

    mob next [ci-skip] [ci skip] [skip ci]

--- src/misc/name-normalizer.test.js
+++ src/misc/name-normalizer.test.js
@@ -1,19 +1,19 @@
-import { normalize } from './name-normalizer'
+import { normalize } from "./name-normalizer";
 
-describe('a name normalizer', () => {
-  it('returns empty when passed empty string', () => {
-    expect(normalize('')).toEqual('')
-  })
+describe("a name normalizer", () => {
+  it("returns empty when passed empty string", () => {
+    expect(normalize("")).toEqual("");
+  });
 
-  it('returns single word name', () => {
-    expect(normalize('Plato')).toEqual('Plato')
-  })
+  it("returns single word name", () => {
+    expect(normalize("Plato")).toEqual("Plato");
+  });
 
-  it('swaps first and last names', () => {
-    expect(normalize('Haruki Murakami')).toEqual('Murakami, Haruki')
-  })
+  it("swaps first and last names", () => {
+    expect(normalize("Haruki Murakami")).toEqual("Murakami, Haruki");
+  });
 
-  xit('trims leading and trailing whitespace', () => {
-    expect(normalize('  Big Boi   ')).toEqual('Boi, Big')
-  })
-})
+  it("trims leading and trailing whitespace", () => {
+    expect(normalize("  Big Boi   ")).toEqual("Boi, Big");
+  });
+});
