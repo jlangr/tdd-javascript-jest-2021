e929954a196f816b74f3131df23df61df6bd218b
Michael Antry <michael.antry.ext@bayer.com>
2021-04-20 15:57:02 -0500

    mob next [ci-skip] [ci skip] [skip ci]

--- src/misc/portfolio.js
+++ src/misc/portfolio.js
@@ -1,35 +1,39 @@
-export const value = undefined
+export const value = (portfolio, stockService) =>
+  isEmpty(portfolio) ? 0 : stockService();
 
 export const sharesOf = (portfolio, symbol) =>
-  (symbol in portfolio.holdings) ? portfolio.holdings[symbol] : 0
+  symbol in portfolio.holdings ? portfolio.holdings[symbol] : 0;
 
-export const uniqueSymbolCount = portfolio => Object.keys(portfolio.holdings).length
+export const uniqueSymbolCount = (portfolio) =>
+  Object.keys(portfolio.holdings).length;
 
-export const create = () => ({ holdings: {} })
+export const create = () => ({ holdings: {} });
 
-const throwOnNonPositivePurchase = shares => {
-  if (shares <= 0) throw new RangeError()
-}
+const throwOnNonPositivePurchase = (shares) => {
+  if (shares <= 0) throw new RangeError();
+};
 
 const transact = (portfolio, symbol, shares) => {
   return {
-    holdings: {...portfolio.holdings, [symbol]: sharesOf(portfolio, symbol) + shares }
-  }
-}
+    holdings: {
+      ...portfolio.holdings,
+      [symbol]: sharesOf(portfolio, symbol) + shares,
+    },
+  };
+};
 
 export const purchase = (portfolio, symbol, shares) => {
-  throwOnNonPositivePurchase(shares)
-  return transact(portfolio, symbol, shares)
-}
+  throwOnNonPositivePurchase(shares);
+  return transact(portfolio, symbol, shares);
+};
 
 export const sell = (portfolio, symbol, shares) => {
-  let newPortfolio = transact(portfolio, symbol, -shares)
+  let newPortfolio = transact(portfolio, symbol, -shares);
   if (sharesOf(newPortfolio, symbol) === 0)
-    delete newPortfolio.holdings[symbol]
-  return newPortfolio
-}
+    delete newPortfolio.holdings[symbol];
+  return newPortfolio;
+};
 
-export const isEmpty = portfolio => {
-  return uniqueSymbolCount(portfolio) === 0
-}
-
+export const isEmpty = (portfolio) => {
+  return uniqueSymbolCount(portfolio) === 0;
+};
