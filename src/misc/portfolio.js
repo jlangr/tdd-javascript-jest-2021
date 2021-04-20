18bbcaaf53a6c95e12cc1b2c190dfec0e23ffa45
Jeff L <jeff@langrsoft.com>
2021-04-20 14:49:09 -0600

    portfolio starter code

---
+++ src/misc/portfolio.js
@@ -0,0 +1,35 @@
+export const value = undefined
+
+export const sharesOf = (portfolio, symbol) =>
+  (symbol in portfolio.holdings) ? portfolio.holdings[symbol] : 0
+
+export const uniqueSymbolCount = portfolio => Object.keys(portfolio.holdings).length
+
+export const create = () => ({ holdings: {} })
+
+const throwOnNonPositivePurchase = shares => {
+  if (shares <= 0) throw new RangeError()
+}
+
+const transact = (portfolio, symbol, shares) => {
+  return {
+    holdings: {...portfolio.holdings, [symbol]: sharesOf(portfolio, symbol) + shares }
+  }
+}
+
+export const purchase = (portfolio, symbol, shares) => {
+  throwOnNonPositivePurchase(shares)
+  return transact(portfolio, symbol, shares)
+}
+
+export const sell = (portfolio, symbol, shares) => {
+  let newPortfolio = transact(portfolio, symbol, -shares)
+  if (sharesOf(newPortfolio, symbol) === 0)
+    delete newPortfolio.holdings[symbol]
+  return newPortfolio
+}
+
+export const isEmpty = portfolio => {
+  return uniqueSymbolCount(portfolio) === 0
+}
+
---
+++ src/misc/portfolio.test.js
@@ -0,0 +1,103 @@
+import * as Portfolio from './portfolio'
+
+describe('a portfolio', () => {
+  let portfolio
+
+  describe('before any purchases made', () => {
+    beforeEach(() => {
+      portfolio = Portfolio.create()
+    })
+
+    it('is empty', () => {
+      expect(Portfolio.isEmpty(portfolio)).toBe(true)
+    })
+
+    it('has a unique symbol count of 0', () => {
+      expect(Portfolio.uniqueSymbolCount(portfolio)).toEqual(0)
+    })
+
+    it('returns 0 for shares of a symbol', () => {
+      expect(Portfolio.sharesOf(portfolio, 'BAYN')).toEqual(0)
+    })
+
+    it('throws when buying non-positive shares', () => {
+      expect(() => {
+        Portfolio.purchase(portfolio, '', 0)
+      }).toThrow(RangeError)
+    })
+  })
+
+  describe('after purchasing a symbol', () => {
+    beforeEach(() => {
+      portfolio = Portfolio.create()
+      portfolio = Portfolio.purchase(portfolio, 'BAYN', 42)
+    })
+
+    it('is not empty', () => {
+      expect(Portfolio.isEmpty(portfolio)).toBe(false)
+    })
+
+    it('has a unique symbol count of 1', () => {
+      expect(Portfolio.uniqueSymbolCount(portfolio)).toEqual(1)
+    })
+
+    it('returns shares for that symbol', () => {
+      expect(Portfolio.sharesOf(portfolio, 'BAYN')).toEqual(42)
+    })
+
+    it('decreases shares when selling', () => {
+      const newPortfolio = Portfolio.sell(portfolio, 'BAYN', 12)
+
+      expect(Portfolio.sharesOf(newPortfolio, 'BAYN')).toEqual(30)
+    })
+
+    it('removes symbol when selling all', () => {
+      const newPortfolio = Portfolio.sell(portfolio, 'BAYN', 42)
+
+      expect(Portfolio.uniqueSymbolCount(newPortfolio, 'BAYN')).toEqual(0)
+    })
+  })
+
+  describe('after purchasing different symbol', () => {
+    beforeEach(() => {
+      portfolio = Portfolio.create()
+      portfolio = Portfolio.purchase(portfolio, 'BAYN', 12)
+      portfolio = Portfolio.purchase(portfolio, 'AAPL', 1)
+    })
+
+    it('increments the unique symbol count', () => {
+      expect(Portfolio.uniqueSymbolCount(portfolio)).toEqual(2)
+    })
+
+    it('returns shares for that symbol', () => {
+      expect(Portfolio.sharesOf(portfolio, 'BAYN')).toEqual(12)
+    })
+  })
+
+  describe('after purchasing same symbol', () => {
+    beforeEach(() => {
+      portfolio = Portfolio.create()
+      portfolio = Portfolio.purchase(portfolio, "BAYN", 10)
+      portfolio = Portfolio.purchase(portfolio, "BAYN", 1)
+    })
+
+    it('does not increment the unique symbol count', () => {
+      expect(Portfolio.uniqueSymbolCount(portfolio)).toEqual(1)
+    })
+
+    it('returns accumulated shares from purchases', () => {
+      expect(Portfolio.sharesOf(portfolio, 'BAYN')).toEqual(11)
+    })
+  })
+
+  describe('value', () => {
+    beforeEach(() => {
+      portfolio = Portfolio.create()
+    })
+
+    it('has value of 0 when portfolio is empty', () => {
+      expect(Portfolio.value(portfolio)).toEqual(0)
+    })
+
+  })
+})
