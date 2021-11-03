e3a284b89cc4720aae0b74623a59edb99daf7993
Jeff L <jeff@langrsoft.com>
2021-11-03 07:30:26 -0600

    new exercise - country and language

---
+++ src/misc/country.js
@@ -0,0 +1,27 @@
+export const Countries = {
+  AUT: [{ language: 'Deutsch', code: 'DE', title: 'Sprache' }],
+  BEL: [{ language: 'Nederlands', code: 'DL', title: 'Taal' },
+    { language: 'Française', code: 'FR', title: 'Langue' }],
+  CAN: [{ language: 'Française', code: 'FR', title: 'Langue' }],
+  CZE: [{ language: 'Český', code: 'CS', title: 'Jazyk' }],
+  DNK: [{ language: 'Dansk', code: 'DA', title: 'Sprog' }],
+  FIN: [{ language: 'Suomalainen', code: 'FI', title: 'Kieli' }],
+  FRA: [{ language: 'Française', code: 'FR', title: 'Langue' }],
+  DEU: [{ language: 'Deutsch', code: 'DE', title: 'Sprache' }],
+  GRC: [{ language: 'Ελληνικά', code: 'EL', title: 'Γλώσσα' }],
+  HUN: [{ language: 'Magyar', code: 'HU', title: 'Nyelv' }],
+  ITA: [{ language: 'Italiano', code: 'IT', title: 'Linguaggio' }],
+  NLD: [{ language: 'Nederlands', code: '', title: 'Taal' }],
+  NOR: [{ language: 'Ungarsk', code: 'NO', title: 'Språk' }],
+  POL: [{ language: 'Polskie', code: 'PL', title: 'Język' }],
+  PRT: [{ language: 'Português', code: 'PT', title: 'Língua' }],
+  ROU: [{ language: 'Română', code: 'RO', title: 'Limba' }],
+  ESP: [{ language: 'Español', code: 'ES', title: 'Idioma' }],
+  SWE: [{ language: 'Svenska', code: 'SV', title: 'Språk' }],
+  CHE: [{ language: 'Deutsch', code: 'DE', title: 'Sprache' },
+    { language: 'Française', code: 'FR', title: 'Langue' },
+    { language: 'Italiano', code: 'IT', title: 'Linguaggio' }]
+};
+
+export const languageTag = _ => {
+}
