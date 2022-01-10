91d7d483313fa8083bbe1fa925bce14ee082f669
Nicholas Myers <ellyh@WUST038965.bayer.cnb>
2022-01-10 16:18:34 -0600

    mob next [ci-skip] [ci skip] [skip ci]

--- src/misc/portfolio.js
+++ src/misc/portfolio.js
@@ -1,20 +1,22 @@
 export const create = () => {
-    let portfolio = {
-        uniqueSymbols: []
-    }
-  return { uniqueSymbols: new Set() }
+  return { uniqueSymbols: new Set(), symbols: {} }
 }
 
 export const uniqueSymbolCount = (portfolio) => {
   return portfolio.uniqueSymbols.size
 }
 
-export const getSymbolShares = () => {
-
+export const getSymbolShares = (portfolio, symbol) => {
+  return portfolio.symbols
 }
  
-export const purchase = (portfolio, symbol, _num) => {
+export const purchase = (portfolio, symbol, num) => {
   let updatedUniqueSymbols = new Set(portfolio.uniqueSymbols)
   updatedUniqueSymbols.add(symbol)
+
+  let countCurrentSymbol = portfolio.symbols[symbol] ?? 0
+  countCurrentSymbol += num
+  portfolio.symbols[symbol] = countCurrentSymbol
+
   return { ...portfolio, uniqueSymbols: updatedUniqueSymbols }
 }
