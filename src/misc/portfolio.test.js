488b59c0455f18c5bfb2d0f8b74e9b07af03a241
Daniel Knight <daniel.knight@monsanto.com>
2022-01-10 15:59:44 -0600

    mob next [ci-skip] [ci skip] [skip ci]

--- src/misc/portfolio.test.js
+++ src/misc/portfolio.test.js
@@ -8,24 +8,24 @@ describe('shares portfolio', () => {
     portfolio = create()
   })
 
-  it('0 count when no purchases made', () => {
+  it('does not have any symbols when first created', () => {
     expect(uniqueSymbolCount(portfolio)).toBe(0)
   })
 
-  it('1 unique symbol after 1 purchase', () => {
+  it('has one unique symbol after purchasing first symbol', () => {
     let newPortfolio = purchase(portfolio, "IBM", 16)
 
     expect(uniqueSymbolCount(newPortfolio)).toBe(1)
   })
 
-  it('2 unique symbol after purchase of additional symbol', () => {
+  it('increases unique symbol count by one when purchasing an additional symbol', () => {
     let newPortfolio = purchase(portfolio, "IBM", 16)
     newPortfolio = purchase(newPortfolio, "AAPL", 10)
 
     expect(uniqueSymbolCount(newPortfolio)).toBe(2)
   })
 
-  it('1 unique symbol after purchasing 2 of the same symbol', () => {
+  it('does not increase symbol count when purchasing same symbol', () => {
     let newPortfolio = purchase(portfolio, "IBM", 16)
     newPortfolio = purchase(newPortfolio, "IBM", 10)
 
