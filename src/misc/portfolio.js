f362457a9d8172362d835d5a6c42606a0963098a
Leah Harwell <leah.harwell.ext@bayer.com>
2022-01-11 09:23:55 -0600

    pull portfolio example from jeff

--- src/misc/portfolio.js
+++ src/misc/portfolio.js
@@ -1,15 +1,21 @@
-export const create = () => ({uniqueSymbols: []});
+export const create = () => ({ sharesBySymbol: {} })
 
-export const purchase = (portfolio, symbol, _numberOfShares) => {
-  //TODO make the set be a set of objects, not just symbols
-  const newUniqueSymbols = [ ...portfolio.uniqueSymbols ];
-  let currentShares = newUniqueSymbols.filter(x => x.symbol === symbol);
-  if (currentShares) currentShares.numberOfShares += _numberOfShares;
-  newUniqueSymbols.add(symbol);
-  return { ...portfolio, uniqueSymbols: newUniqueSymbols }
-};
+const validate = numberOfShares => { if (numberOfShares <= 0) throw new Error(); };
 
-export const uniqueSymbolCount = portfolio => portfolio.uniqueSymbols.size;
+export const purchase = (portfolio, symbol, numberOfShares) => {
+  validate(numberOfShares);
+  return {
+    ...portfolio,
+    sharesBySymbol: {
+      ...portfolio.sharesBySymbol,
+      [symbol]: sharesOf(portfolio, symbol) + numberOfShares
+    }
+  }
+}
 
-export const numberOfShares = (portfolio, symbol) => portfolio.uniqueSymbols[symbol];
+export const uniqueSymbolCount = portfolio =>
+  Object.keys(portfolio.sharesBySymbol).length
+
+export const sharesOf = (portfolio, symbol) => portfolio.sharesBySymbol[symbol] || 0
+
 
--- src/misc/portfolio.test.js
+++ src/misc/portfolio.test.js
@@ -13,35 +13,63 @@ describe('a portfolio', () => {
     })
 
     it('is one after single purchase', () => {
-      const newPortfolio = Portfolio.purchase(portfolio, "BAYN", 10)
+      const newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 10)
 
       expect(Portfolio.uniqueSymbolCount(newPortfolio)).toEqual(1)
     })
 
     it('increments for each distinct symbol purchase', () => {
-      let newPortfolio = Portfolio.purchase(portfolio, "BAYN", 10)
+      let newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 10)
 
-      newPortfolio = Portfolio.purchase(newPortfolio, "IBM", 10)
+      newPortfolio = Portfolio.purchase(newPortfolio, 'IBM', 10)
 
       expect(Portfolio.uniqueSymbolCount(newPortfolio)).toEqual(2)
     })
 
     it('does not increment for same-symbol purchase', () => {
-      let newPortfolio = Portfolio.purchase(portfolio, "BAYN", 10)
+      let newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 10)
 
-      newPortfolio = Portfolio.purchase(newPortfolio, "BAYN", 10)
+      newPortfolio = Portfolio.purchase(newPortfolio, 'BAYN', 10)
 
       expect(Portfolio.uniqueSymbolCount(newPortfolio)).toEqual(1)
     })
+  })
 
-    it('returns 0 for number of shares if you have not purchased anything', () => {
-      expect(Portfolio.numberOfShares(portfolio)).toBe(0);
-    });
+  describe('shares of symbol', () => {
+    it('returns 0 for shares of unpurchased symbol', () => {
+      expect(Portfolio.sharesOf(portfolio, 'BAYN')).toEqual(0)
+    })
 
-    it('returns the correct number of shares after making a single purchase', () => {
-      let newPortfolio = Portfolio.purchase(portfolio, "BAYN", 10);
-      expect(Portfolio.numberOfShares(newPortfolio, "BAYN")).toBe(10);
-    });
+    it('returns shares for single symbol purchase', () => {
+      const newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 42)
+
+      expect(Portfolio.sharesOf(newPortfolio, 'BAYN')).toEqual(42)
+    })
+
+    it('returns shares for symbol', () => {
+      let newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 42)
+      newPortfolio = Portfolio.purchase(newPortfolio, 'IBM', 100)
+
+      expect(Portfolio.sharesOf(newPortfolio, 'BAYN')).toEqual(42)
+    })
+
+    it('adds shares for same-symbol purchase', () => {
+      let newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 42)
+      newPortfolio = Portfolio.purchase(newPortfolio, 'BAYN', 100)
+
+      expect(Portfolio.sharesOf(newPortfolio, 'BAYN')).toEqual(142)
+    })
+  })
+
+  describe('purchase constraints', () => {
+    [
+      { shares: 0, info: 'zero disallowed' },
+      { shares: -10, info: 'negative numbers disallowed' },
+    ].forEach(({ shares, _info }) => {
+      it('barfs on purchase of 0 shares', () => {
+        expect(() => Portfolio.purchase(portfolio, 'BAYN', shares)).toThrow()
+      })
+    })
   })
 
 })
