d5d350800740049a38c2bfa45aef724aed42c50c
TEATER <cteat@monsanto.com>
2022-01-11 11:30:30 -0600

    mob next [ci-skip] [ci skip] [skip ci]

--- src/misc/roman-converter.test.js
+++ src/misc/roman-converter.test.js
@@ -1,18 +1,10 @@
 import { convert } from './roman-converter'
 
 describe('roman converter test', () => {
-
-  it("can convert 1 to roman numeral", () => {
-    expect(convert(1)).toBe("I")
-  })
-
-  it("can convert 3 to roman numeral", () => {
-    expect(convert(3)).toBe("III")
-  })
-
-  [{number:1, roman:"I"}].forEach({number, roman} => {
-    it("can convert 1 to roman numeral", () => {
-      expect(convert(1)).toBe("I")
+  let testArray = [{number:1, roman:"I"}, {number: 3, roman: "III"}]
+  testArray.forEach(({number, roman}) => {
+    it(`can convert ${number} to roman numeral`, () => {
+      expect(convert(number)).toBe(roman)
     })
   });
 })
