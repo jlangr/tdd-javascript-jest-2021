1c86b3685f155737fd98efe251eb24df899eee27
Leah Harwell <leah.harwell.ext@bayer.com>
2022-01-11 09:01:33 -0600

    Squashed commit of the following:
    commit 7b3b34313a63a7c3077ec3877ead05d12975f2ce
    Author: Leah Harwell <leah.harwell.ext@bayer.com>
    Date:   Tue Jan 11 09:01:14 2022 -0600
    
        mob next [ci-skip] [ci skip] [skip ci]

--- src/misc/portfolio.js
+++ src/misc/portfolio.js
@@ -1,8 +1,10 @@
-export const create = () => ({uniqueSymbols: new Set()});
+export const create = () => ({uniqueSymbols: []});
 
 export const purchase = (portfolio, symbol, _numberOfShares) => {
   //TODO make the set be a set of objects, not just symbols
-  const newUniqueSymbols = new Set(portfolio.uniqueSymbols)
+  const newUniqueSymbols = [ ...portfolio.uniqueSymbols ];
+  let currentShares = newUniqueSymbols.filter(x => x.symbol === symbol);
+  if (currentShares) currentShares.numberOfShares += _numberOfShares;
   newUniqueSymbols.add(symbol);
   return { ...portfolio, uniqueSymbols: newUniqueSymbols }
 };
